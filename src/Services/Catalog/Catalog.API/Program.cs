875e4d2f3088dda75a1381cfe54daab197170fbd
Mohamed Nabarawy <m.gamal.nabarawy@gmail.com>
2023-12-29 15:44:53 +0200

    adding controls

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -6,10 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ICatalogContext, CatalogContext>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
-
+builder.Services.AddMvcCore();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,7 +21,8 @@ if (app.Environment.IsDevelopment())
 }
 
 
-
 app.Run();
+
+
 
 
