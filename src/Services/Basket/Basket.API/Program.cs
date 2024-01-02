95ae63bfc4db95083e4b70a2c2f00c0a65a18e55
Mohamed Nabarawy <m.gamal.nabarawy@gmail.com>
2024-01-02 14:51:24 +0200

    add redis cash service

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,10 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Extensions.Options;
 
+var builder = WebApplication.CreateBuilder(args);
+var configValue = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = configValue;
+});
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,5 +20,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
+app.MapControllers();
 app.Run();
 
