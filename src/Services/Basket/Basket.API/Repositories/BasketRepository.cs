370f42f4dbcbbff6ad81144b553b1b605e6e1fee
Mohamed Nabarawy <m.gamal.nabarawy@gmail.com>
2024-01-02 15:36:06 +0200

    Adding Basket Repository

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,39 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache;
+        }
+
+        public Task DeleteBasketAsync(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ShoppingCart> GetBasketAsync(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+
+            if (String.IsNullOrEmpty(basket))
+                return null;
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+        public async Task<ShoppingCart> UpdateBasketAsync(ShoppingCart basket)
+        {
+            await _redisCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            return await GetBasketAsync(basket.UserName);
+        }
+    }
+}
