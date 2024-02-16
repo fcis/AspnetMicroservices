fd58d79ad1465930d6de1bdbef2b05672364cfec
Mohamed Nabarawy <m.gamal.nabarawy@gmail.com>
2024-02-16 20:38:30 +0200

    InfrastructureServiceRegistration

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,10 +1,13 @@
+using Ordering.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddApplicationServices();
+//builder.Services.AddInfrastructureServices(ConfigurationBuilder);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
