5e2568ecb3c7406db7126c50db7deaab5a484e3d
Mohamed Nabarawy <m.gamal.nabarawy@gmail.com>
2024-02-16 19:51:10 +0200

    Email Service

--- src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -24,7 +24,7 @@ namespace Ordering.Infrastructure.Persistence
         {
             return new List<Order>
             {
-                new Order() {UserName = "swn", FirstName = "Mehmet", LastName = "Ozkaya", EmailAddress = "ezozkme@gmail.com", AddressLine = "Bahcelievler", Country = "Turkey", TotalPrice = 350 }
+                new Order() {UserName = "swn", FirstName = "mohamd", LastName = "nabarawy", EmailAddress = "m.gamal.nabarawy@gmail.com", AddressLine = "Bahcelievler", Country = "Turkey", TotalPrice = 350 }
             };
         }
     }
