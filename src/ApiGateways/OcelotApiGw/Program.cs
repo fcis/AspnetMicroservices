826eb38567a86678e128291f5c26d0c5e73c0262
Mohamed Nabarawy <m.gamal.nabarawy@gmail.com>
2024-03-01 19:53:41 +0200

    ocelot configurations

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,6 +1,15 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging.AddConsole();
+
+builder.Logging.AddDebug();
+builder.Services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.UseRouting();
+//app.MapGet("/", () => "Hello World!");
+await app.UseOcelot();
 app.Run();
